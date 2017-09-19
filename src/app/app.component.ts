41bbb58f03d424f03c185cc5e11dfff0c76db486
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-09-19 09:35:45 +0300

    morning refactoring, fixed footer I broke last time, and made the app.component look a bit better.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, QueryList, ViewChildren } from '@angular/core';
-import { DrawerComponent, DrawerMode } from './modules/drawer/components/drawer/drawer.component';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { DrawerMode } from './modules/drawer/components/drawer/drawer.component';
 import { DocumentService, FormFactor } from './services/document.service';
 
 @Component({
@@ -7,43 +7,47 @@ import { DocumentService, FormFactor } from './services/document.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChildren(DrawerComponent) drawers: QueryList<DrawerComponent>;
-
+export class AppComponent implements OnInit, AfterViewInit {
   private formFactor: FormFactor;
-  width: string;
-  docked: boolean;
-  mode: DrawerMode;
-  hideFooter: boolean;
+
+  drawerWidth: string;
+  isDrawerDocked: boolean;
+  drawerMode: DrawerMode;
+  showFooter: boolean;
   isFooterTransitioning: boolean;
 
-  constructor(private document$: DocumentService) {
-    this.formFactor = this.document$.formFactor$.getValue();
+  constructor(private document$: DocumentService) {}
 
-    this.docked = this.setDrawerDocked(this.formFactor);
-    this.hideFooter = !this.showFooter(this.formFactor);
-    this.width = this.setDrawerWidth(this.formFactor);
-    this.mode = this.setDrawerMode(this.formFactor);
+  ngOnInit(): void {
+    this.formFactor = this.document$.formFactor$.getValue();
+    this.updateDrawer(this.formFactor);
   }
 
   ngAfterViewInit(): void {
     this.document$.formFactor$
       .filter(formFactor => formFactor !== this.formFactor)
-      .subscribe(formFactor => {
+      .subscribe((formFactor: FormFactor) => {
         this.formFactor = formFactor;
-
-        this.docked = this.setDrawerDocked(formFactor);
-        this.hideFooter = !this.showFooter(formFactor);
-        this.width = this.setDrawerWidth(formFactor);
-        this.mode = this.setDrawerMode(formFactor);
+        this.updateDrawer(formFactor);
       });
+  }
+
+  private updateDrawer(formFactor: FormFactor): void {
+    this.drawerWidth = this.setDrawerWidth(formFactor);
+    this.isDrawerDocked = this.setDrawerDocked(formFactor);
+    this.drawerMode = this.setDrawerMode(formFactor);
+    const show = this._showFooter(formFactor);
+    if (show !== this.showFooter) {
+      this.isFooterTransitioning = true;
+    }
+    this.showFooter = show;
   }
 
   private setDrawerDocked(formFactor: FormFactor): boolean {
     return formFactor === FormFactor.MEDIUM || formFactor === FormFactor.LARGE;
   }
 
-  private showFooter(formFactor: FormFactor): boolean {
+  private _showFooter(formFactor: FormFactor): boolean {
     return formFactor === FormFactor.SMALL;
   }
 
